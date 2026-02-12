a264a1e7b4fac5006f7e588da4bbe6fb9729a2dd
StunnyBG <stunnybg@gmail.com>
2026-02-12 16:27:04 +0200

    Add project files.

---
+++ Tehnicharche/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Tehnicharche.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
