58e1774a0c8cb1258f739aece24aeb839f2c4bff
StunnyBG <stunnybg@gmail.com>
2026-03-29 15:39:01 +0300

    Add IsBanned flag to ApplicationUser
    this will be used in the future to ban users

--- Tehnicharche.Data.Models/ApplicationUser.cs
+++ Tehnicharche.Data.Models/ApplicationUser.cs
@@ -5,6 +5,8 @@ namespace Tehnicharche.Data.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        public bool IsBanned { get; set; }
+
         public virtual ICollection<Listing> Listings { get; set; } = new HashSet<Listing>();
     }
 }
